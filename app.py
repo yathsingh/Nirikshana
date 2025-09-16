f03f4451cc26c6388efae9c1005667e99c7295ca
yathsingh <genius.yatharth@gmail.com>
2025-09-16 14:09:28 +0530

    Fix

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import threading
 
 from backend.db import SessionLocal, init_db, WaterData, Alert  
 from backend.predict import predict_water_quality
-from backend.simulator import simulate_live
+from backend.simulate_live import simulate_live
 
 init_db()
 
